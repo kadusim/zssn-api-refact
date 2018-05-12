9e225fd10a180daedfe167a84f52432227f20f16
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-05-12 03:17:53 -0300

    Refactory service Exchange and add ApplicationService

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,7 +2,7 @@ class ExchangesController < ApplicationController
 
   api :POST, 'api/exchanges', 'Exchange Resources'
   def create
-    ExchangerService.new(exchange_parms).call
+    ExchangerService.call(exchange_parms)
     json_response({ message: 'Successful exchange' })
   end
 
--- app/services/exchanger_service.rb
+++ app/services/exchanger_service.rb
@@ -1,15 +1,12 @@
-class ExchangerService
-  attr_reader :exchange_parms, :resources, :survivor_one, :survivor_two, :survivor_one_change_resources, :survivor_two_change_resources
+class ExchangerService < ApplicationService
+  attr_reader :resources, :survivor_one, :survivor_two, :survivors_change_resources
 
   def initialize(exchange_parms)
-    @exchange_parms                = exchange_parms
     @resources                     = Resource.all
 
     @survivor_one                  = Survivor.find(exchange_parms[:survivor_one][:id])
-    @survivor_one_change_resources = exchange_parms[:survivor_one][:resources]
-
     @survivor_two                  = Survivor.find(exchange_parms[:survivor_two][:id])
-    @survivor_two_change_resources = exchange_parms[:survivor_two][:resources]
+    @survivors_change_resources    = exchange_parms[:survivor_one], exchange_parms[:survivor_two]
   end
 
   def call
@@ -20,33 +17,37 @@ class ExchangerService
 
   def exchange!
     InventoryResource.transaction do
+      survivor_one_change_resources = survivors_change_resources[0][:resources]
+      survivor_two_change_resources = survivors_change_resources[1][:resources]
       exchange_resources!(survivor_change_resources: survivor_one_change_resources, survivor_of: survivor_one, survivor_to: survivor_two)
       exchange_resources!(survivor_change_resources: survivor_two_change_resources, survivor_of: survivor_two, survivor_to: survivor_one)
     end
   end
 
   def survivors_can_exchange_resources?
-    check_survivors_infected(survivor_one)
-    check_survivors_infected(survivor_two)
+    check_survivors_infected
+    check_survivors_have_enough_resources
+    check_survivors_values_items_matching(sum_points_resources_change)
 
-    check_survivors_have_enough_items(survivor_one_change_resources, survivor_one)
-    check_survivors_have_enough_items(survivor_two_change_resources, survivor_two)
-
-    total_points_survivor_one = sum_points_resources_change(survivor_one_change_resources, survivor_two)
-    total_points_survivor_two = sum_points_resources_change(survivor_two_change_resources, survivor_two)
-
-    check_survivors_values_items_matching(total_points_survivor_one, total_points_survivor_two)
     return true
   end
 
-  def check_survivors_infected(survivor)
-    if survivor.infected?
-      raise ExceptionHandler::SurviroIsInfected, ("Be careful, survivor id: #{survivor.id} is infected")
+  def check_survivors_infected
+    survivors.each do |survivor|
+      if survivor.infected?
+        raise ExceptionHandler::SurviroIsInfected, ("Be careful, survivor id: #{survivor.id} is infected")
+      end
     end
   end
 
-  def check_survivors_have_enough_items(survivor_change_resources, survivor)
-    survivor_change_resources.each do |resource_change|
+  def check_survivors_have_enough_resources
+    survivors.each_with_index do |survivor, index|
+      check_enough_resources(survivor, index)
+    end
+  end
+
+  def check_enough_resources(survivor, index)
+    survivors_change_resources[index][:resources].each do |resource_change|
 
       quantity_resources_change    = resource_change[:quantity].to_i
       quantity_resources_inventory = survivor.inventory_resources.where(resource_id: resource_change[:resource_id]).count
@@ -58,19 +59,23 @@ class ExchangerService
     end
   end
 
-  def sum_points_resources_change(survivor_change_resources, survivor)
-    resources_survivor_total_value = 0
+  def sum_points_resources_change
+    resources_survivors_total_values = [0, 0]
 
-    survivor_change_resources.map do |resource_change|
-      resource_value                  = resources.find(resource_change[:resource_id]).value
-      resources_survivor_total_value += resource_change[:quantity].to_i * resource_value
+    survivors.each_with_index do |survivor, index|
+
+      survivors_change_resources[index][:resources].each do |resource_change|
+        resource_value                           = resources.find(resource_change[:resource_id]).value
+        resources_survivors_total_values[index] += resource_change[:quantity].to_i * resource_value
+      end
+
     end
 
-    resources_survivor_total_value
+    resources_survivors_total_values
   end
 
-  def check_survivors_values_items_matching(total_points_survivor_one, total_points_survivor_two)
-    if total_points_survivor_one != total_points_survivor_two
+  def check_survivors_values_items_matching(resources_survivors_total_values)
+    if resources_survivors_total_values[0] != resources_survivors_total_values[1]
       raise ExceptionHandler::ResourcesNotMatching, ("An exchange must be made with the same amount of points")
     end
   end
@@ -89,6 +94,10 @@ class ExchangerService
         survivor_to.inventory.inventory_resources.create!({resource: resource_included})
       end
     end
+  end
+
+  def survivors
+    [survivor_one, survivor_two]
   end
 
 end
